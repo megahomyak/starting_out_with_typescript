447631e4292cdbdadac9af27a1e02625284269e7
megahomyak <g.megahomyak@gmail.com>
2024-09-07 15:38:38 +0500

    Connected some libraries, wrote a simple Telegram bot

--- dist/hello.mjs
+++ dist/hello.mjs
@@ -1,2 +1,13 @@
-import { sayHello } from "./hello2.mjs";
-sayHello();
+import TelegramBot from "node-telegram-bot-api";
+console.log(TelegramBot);
+import * as fs from "fs";
+import * as s from "superstruct";
+const Config = s.object({
+    token: s.string(),
+});
+const config = s.create(JSON.parse(fs.readFileSync("config.json", "utf-8")), Config);
+const bot = new TelegramBot(config.token);
+bot.on("message", (message) => {
+    console.log(message.text);
+});
+bot.startPolling();
--- src/hello.mts
+++ src/hello.mts
@@ -1,3 +1,19 @@
-import { sayHello } from "./hello2.mjs";
+import TelegramBot from "node-telegram-bot-api";
 
-sayHello();
+import * as fs from "fs";
+
+import * as s from "superstruct";
+
+const Config = s.object({
+    token: s.string(),
+});
+
+const config = s.create(JSON.parse(fs.readFileSync("config.json", "utf-8")), Config);
+
+const bot = new TelegramBot(config.token);
+
+bot.on("message", (message) => {
+    console.log(message.text);
+});
+
+bot.startPolling();
